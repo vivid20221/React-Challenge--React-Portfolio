566b6c024454cef5e242d5b761fee9eb2725d875
juliain jauregui <julainj9011@gmail.com>
2023-02-20 00:09:07 -0600

    scss for side bar

--- react-portfilo/src/components/Sidebar/index.js
+++ react-portfilo/src/components/Sidebar/index.js
@@ -3,7 +3,7 @@ import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubsitle from '../../assets/images/logo_sub.png'
 import {FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 
 
@@ -19,10 +19,10 @@ import { faHome } from '@fortawesome/free-solid-svg-icons'
                 <NavLink exact="true" activeclassname="active" to="/">
                     <FontAwesomeIcon icon={faHome} color= "#4d4d4e" />
                 </NavLink>
-                <NavLink exact="true" activeclassname="active" to="/">
-                    <FontAwesomeIcon icon={faHome} color= "#4d4d4e" />
-                </NavLink><NavLink exact="true" activeclassname="active" to="/">
-                    <FontAwesomeIcon icon={faHome} color= "#4d4d4e" />
+                <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                    <FontAwesomeIcon icon={faUser} color= "#4d4d4e" />
+                </NavLink><NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                    <FontAwesomeIcon icon={faEnvelope} color= "#4d4d4e" />
                 </NavLink>
             </nav>
     </div>

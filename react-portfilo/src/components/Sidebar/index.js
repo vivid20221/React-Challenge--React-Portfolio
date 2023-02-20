132634d45dd810ad27d93e8f247dcc149efb767e
juliain jauregui <julainj9011@gmail.com>
2023-02-19 23:44:52 -0600

    working on layout and side bar

--- react-portfilo/src/components/Sidebar/index.js
+++ react-portfilo/src/components/Sidebar/index.js
@@ -1,5 +1,30 @@
+import {Link, NavLink} from 'react-router-dom'
 import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubsitle from '../../assets/images/logo_sub.png'
+import {FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 
-const Sidebar = () => <>Hello from sidebar </>
 
+
+
+
+ const Sidebar = () => (
+    <div className= 'nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt="logo"/>
+            <img className="sub-logo" src={LogoSubsitle} alt="slobodan" />
+            </Link>
+            <nav>
+                <NavLink exact="true" activeclassname="active" to="/">
+                    <FontAwesomeIcon icon={faHome} color= "#4d4d4e" />
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" to="/">
+                    <FontAwesomeIcon icon={faHome} color= "#4d4d4e" />
+                </NavLink><NavLink exact="true" activeclassname="active" to="/">
+                    <FontAwesomeIcon icon={faHome} color= "#4d4d4e" />
+                </NavLink>
+            </nav>
+    </div>
+)
 export default Sidebar 
